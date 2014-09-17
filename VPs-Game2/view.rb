1426748259abade2c3c0501b869986e21f3e1101
Vasanth Pappu <vpappu@covermymeds.com>
2014-09-16 21:51:13 -0400

    board printing

--- VPs-Game2/view.rb
+++ VPs-Game2/view.rb
@@ -21,10 +21,19 @@ BLOCK
     end
 
 
-    def moved(dir)
+    def moved(board, xpos, ypos, dir)
+      board[xpos][ypos] = 'O'.green
+      print_board(board)
       puts "moved #{dir}"
     end
 
+    def print_board(board)
+      x = board.size
+      y = board.first.size
+      x.times do |x|
+        puts board[board.size - x - 1].join("")
+      end
+    end
 
 
 
