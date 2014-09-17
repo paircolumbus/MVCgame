2317cd6757e7ab0420803a48a95705f81b0552e4
Vasanth Pappu <vpappu@covermymeds.com>
2014-09-16 21:51:32 -0400

    enforce boundaries

--- VPs-Game2/model.rb
+++ VPs-Game2/model.rb
@@ -1,40 +1,42 @@
 require_relative 'view'
 
-module Model
-  BOARD = 8.times.map do |i|
-    8.times.map do |j|
-      'X'.red
-    end
-  end
+class Model
+  attr_accessor :board, :xpos, :ypos, :xmax, :ymax
 
-  XPOS = 0
-  YPOS = 0
+  def initialize
+    @xmax = 8
+    @ymax = 8
 
-  class << self
-
-
-
-
-
-    def move(dir)
-      case dir
-      when :up
-        YPOS += 1
-      when :down
-        YPOS -= 1
-      when :left
-        XPOS -= 1
-      when :right
-        XPOS += 1
+    @board = @xmax.times.map do |i|
+      @ymax.times.map do |j|
+        'X'.red
       end
-      View::moved(BOARD, XPOS, YPOS)
     end
 
-
-
-
-
+    @xpos = 0
+    @ypos = 0
   end
+
+  def move(dir)
+    @board[@xpos][@ypos] = 'X'.red
+    case dir
+    when :up
+      @ypos = ( @ypos + 1 == @ymax ? @ypos : @ypos + 1 )
+    when :down
+      @ypos = ( @ypos - 1 < 0 ? @ypos : @ypos - 1 )
+    when :left
+      @xpos = ( @xpos - 1 < 0 ? @xpos : @xpos - 1 )
+    when :right
+      @xpos = ( @xpos + 1 == @xmax ? @xpos : @xpos + 1 )
+    end
+    
+    View::moved(@board, @ypos, @xpos, dir)
+  end
+
+
+
+
+
 end
 
 
