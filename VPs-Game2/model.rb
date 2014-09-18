83dccd21afcede89e5e5965588fe573de86a9fa8
Vasanth Pappu <vpappu@covermymeds.com>
2014-09-18 18:36:01 -0400

    made two layers of the board

--- VPs-Game2/model.rb
+++ VPs-Game2/model.rb
@@ -1,7 +1,14 @@
 require_relative 'view'
 
 class Model
-  attr_accessor :board, :xpos, :ypos, :xmax, :ymax
+  attr_accessor :board,
+                :xpos,
+                :ypos,
+                :xmax,
+                :ymax,
+                :word,
+                :is_empty
+
 
   def initialize
     @xmax = 8
@@ -9,16 +16,69 @@ class Model
 
     @board = @xmax.times.map do |i|
       @ymax.times.map do |j|
-        'X'.red
+        {text: 'X'.red,
+         shale: nil}
       end
     end
 
     @xpos = 0
     @ypos = 0
+
+    @word = "hidesay"
+    @is_empty = ->(x,y){@board[x][y][:shale].nil?}
+
+    @surface = -> (){map_square{|s| s[:text]}}
+    @wordloc = intersperse_word(@word)
   end
 
+
+  def each_square()
+    @xmax.times do |x|
+      @ymax.times do |y|
+        yield(@board[x][y])
+      end
+    end
+  end
+
+  def map_square
+    @xmax.times.map do |x|
+      @ymax.times.map do |y|
+        yield(@board[x][y])
+      end
+    end
+  end
+
+
+  def board
+    @surface.()
+  end
+
+
+  def get_a_random_square
+    [rand(@xmax), rand(@ymax)]
+  end
+
+  def get_a_random_empty_square
+    x,y = get_a_random_square
+    if @is_empty.(x,y)
+      [x,y]
+    else
+      get_a_random_empty_square
+    end
+  end
+
+
+  def intersperse_word(word)
+    word.each_char do |c|
+      x,y = get_a_random_empty_square
+      @board[x][y][:shale] = c.upcase.yellow
+    end
+  end
+
+
   def move(dir)
-    @board[@ypos][@xpos] = 'X'.red
+
+    @board[@ypos][@xpos][:text] = 'X'.red
     case dir
     when :up
       @ypos = ( @ypos + 1 == @ymax ? @ypos : @ypos + 1 )
@@ -29,8 +89,8 @@ class Model
     when :right
       @xpos = ( @xpos + 1 == @xmax ? @xpos : @xpos + 1 )
     end
-    
-    View::moved(@board, @ypos, @xpos, dir)
+
+    View::moved(@surface.(), @ypos, @xpos, dir)
   end
 
 
