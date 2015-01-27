fd7bfae7fcd4dd2dae5f885b66dbceb086bbafaa
Jonathan Martindell <jmartindell@covermymeds.com>
2015-01-27 13:04:21 -0500

    Break out print into more modules

--- JonsGame/view.rb
+++ JonsGame/view.rb
@@ -40,49 +40,72 @@ EOS
       end
 
       def print_retirement_comparison(scenario1, scenario2)
-        output = [] 
-        output[0]   = "********************************"
-        output[1]   = ""
-        output[2]   = "********************************"
-        output[3]   = sprintf("%-21s", "Beginning Age:")
-        output[4]   = sprintf("%-21s", "Retirement Age:")
-        output[5]   = sprintf("%-21s", "Age of Death:")
-        output[6]   = sprintf("%-21s", "Annual Contribution:")
-        output[7]   = sprintf("%-21s", "Market Return:")
-        output[8]   = sprintf("%-21s", "Accum. Tax Rate:")
-        output[9]   = sprintf("%-21s", "Retir. Tax Rate:")
-        output[10]  = sprintf("%-21s", "Account Type:")
-        output[11]  = "********************************"
-        output[12]  = sprintf("%-21s", "Retirement Income:")
-        output[13]  = "********************************"
- 
-        output[1]  << sprintf("%-32s", scenario1.name)
-        output[3]  << sprintf("%-12d", scenario1.beginning_age)
-        output[4]  << sprintf("%-12d", scenario1.retirement_age)
-        output[5]  << sprintf("%-12d", scenario1.death_age)
-        output[6]  << sprintf("$%-11d", scenario1.annual_contribution)
-        output[7]  << sprintf("%-12.2f", scenario1.accumulation_market_return_rate)
-        output[8]  << sprintf("%-12.2f", scenario1.accumulation_tax_rate)
-        output[9]  << sprintf("%-12.2f", scenario1.retirement_tax_rate)
-        output[10]  << sprintf("%-12s", scenario1.ira_type.to_s)
-        output[12] << sprintf("$%-11d", scenario1.retirement_income)
+        output = Array.new(14, "")
+        add_frame output
+        add_row_header output
 
-        output[0]  << " ********************************"
-        output[1]  << " %s" % scenario2.name
-        output[2]  << " ********************************"
-        output[3]  << sprintf("%-12d", scenario2.beginning_age)
-        output[4]  << sprintf("%-12d", scenario2.retirement_age)
-        output[5]  << sprintf("%-12d", scenario2.death_age)
-        output[6]  << sprintf("$%-11d", scenario2.annual_contribution)
-        output[7]  << sprintf("%-12.2f", scenario2.accumulation_market_return_rate)
-        output[8]  << sprintf("%-12.2f", scenario2.accumulation_tax_rate)
-        output[9]  << sprintf("%-12.2f", scenario2.retirement_tax_rate)
-        output[10] << sprintf("%-12s", scenario2.ira_type.to_s)
-        output[11] << " ********************************"
-        output[12] << sprintf("$%-11d", scenario2.retirement_income)
-        output[13] << " ********************************"
-
+        add_scenario_output(output, scenario1)
+        add_frame output
+        add_scenario_output(output, scenario2)
         puts output
+        puts results_output(scenario1, scenario2)
+      end
+
+      def add_scenario_output(output, scenario)
+        output[1]  += sprintf("%-32s", scenario.name)
+        output[3]  += sprintf("%-12d", scenario.beginning_age)
+        output[4]  += sprintf("%-12d", scenario.retirement_age)
+        output[5]  += sprintf("%-12d", scenario.death_age)
+        output[6]  += sprintf("$%-11d", scenario.annual_contribution)
+        output[7]  += sprintf("%-12.2f", scenario.accumulation_market_return_rate)
+        output[8]  += sprintf("%-12.2f", scenario.accumulation_tax_rate)
+        output[9]  += sprintf("%-12.2f", scenario.retirement_tax_rate)
+        output[10] += sprintf("%-12s", scenario.ira_type.to_s)
+        output[12] += sprintf("$%-11d", scenario.retirement_income)
+      end
+
+      def add_frame(output)
+        output[0]   += "********************************"
+        output[1]   += ""
+        output[2]   += "********************************"
+        output[3]   += ""
+        output[4]   += ""
+        output[5]   += ""
+        output[6]   += ""
+        output[7]   += ""
+        output[8]   += ""
+        output[9]   += ""
+        output[10]  += ""
+        output[11]  += "********************************"
+        output[12]  += ""
+        output[13]  += "********************************"
+      end
+
+      def add_row_header(output)
+        output[0]   += ""
+        output[1]   += ""
+        output[2]   += ""
+        output[3]   += sprintf("%-21s", "Beginning Age:")
+        output[4]   += sprintf("%-21s", "Retirement Age:")
+        output[5]   += sprintf("%-21s", "Age of Death:")
+        output[6]   += sprintf("%-21s", "Annual Contribution:")
+        output[7]   += sprintf("%-21s", "Market Return:")
+        output[8]   += sprintf("%-21s", "Accum. Tax Rate:")
+        output[9]   += sprintf("%-21s", "Retir. Tax Rate:")
+        output[10]  += sprintf("%-21s", "Account Type:")
+        output[11]  += ""
+        output[12]  += sprintf("%-21s", "Retirement Income:")
+        output[13]  += ""
+      end  
+
+      def results_output(scenario1, scenario2)
+        if scenario1.retirement_income == scenario2.retirement_income
+          "Result: both scenarios generate the same income in retirement."
+        elsif scenario1.retirement_income > scenario2.retirement_income
+          "Result: #{scenario1.name} generates $#{scenario1.retirement_income - scenario2.retirement_income} more income in retirement!"
+        else 
+          "Result: #{scenario2.name} generates $#{scenario2.retirement_income - scenario1.retirement_income} more income in retirement!"
+        end
       end
 
       def serialize_scenario
