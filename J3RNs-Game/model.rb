7877fda63f96b5e265f7302c0ffa6afcc614dca1
Jonathan Arnett <JArnett@covermymeds.com>
2015-05-26 12:42:49 -0400

    Turnary solution

--- J3RNs-Game/model.rb
+++ J3RNs-Game/model.rb
@@ -1,20 +1,16 @@
 class VoteCounter
   attr_reader :items
 
-  # TODO explicit return -> turnary
-
   def initialize
     @items = []
   end
 
   def add_item(name)
-    return false if @items.find { |x| x.name == name }
-    @items << Item.new(name)
+    @items.find { |x| x.name == name } ? false : @items << Item.new(name)
   end
 
   def vote_for(name)
-    return false unless (item = @items.find { |x| x.name == name })
-    item.vote
+    (item = @items.find { |x| x.name == name }) ? item.vote : false
   end
 
   def winning_items
