e4aeb4ff15d362ec229abec91e0d4592f5374c1e
Jonathan Arnett <JArnett@covermymeds.com>
2015-05-26 12:43:03 -0400

    Model commented for clarity

--- J3RNs-Game/model.rb
+++ J3RNs-Game/model.rb
@@ -5,19 +5,23 @@ class VoteCounter
     @items = []
   end
 
+  # Add item if it does not already exist
   def add_item(name)
     @items.find { |x| x.name == name } ? false : @items << Item.new(name)
   end
 
+  # Vote for item if item exists
   def vote_for(name)
     (item = @items.find { |x| x.name == name }) ? item.vote : false
   end
 
+  # Get an array of items sharing the highest number of votes
   def winning_items
     max_votes = @items.map { |y| y.votes }.sort.last
     @items.select { |x| x.votes == max_votes }
   end
 
+  # Delete item from list
   def delete(item)
     @items.reject! { |x| x.name == item }
   end
@@ -31,6 +35,7 @@ class Item
     @votes = 0
   end
 
+  # Votes++
   def vote
     @votes += 1
   end
