f86a8a525078cd43afd55d6d3b8a11e9ef9d44ab
Kaleb Davis <kdavis@covermymeds.com>
2015-06-04 11:48:48 -0400

    core functionality of yahtzee game implemented

---
+++ kalebs-game/controller.rb
@@ -0,0 +1,36 @@
+require_relative 'view'
+require_relative 'model'
+
+class GameController
+  include GameView
+
+  def run!
+    dice = DiceList.new
+
+    Print::run_loader
+    Print::title_screen
+
+    loop do
+      Print::menu
+      case Print::fetch_user_input
+      when "R"
+        dice.roll!
+        dice.each { |die| Print::print_die(die.value) }
+        Print::did_i_win(dice.yahtzee?)
+      # when "X"
+      #   dice.reset!
+      when "C"
+        dice.win!
+        dice.each { |die| Print::print_die(die.value) }
+        Print::did_i_win(dice.yahtzee?)
+      when "Q"
+        puts "You chose to exit"
+        exit
+      else
+        Print::error_message
+      end
+    end
+  end
+end
+
+GameController.new.run!
