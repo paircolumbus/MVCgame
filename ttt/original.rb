b6dcb7bff7b7ec3aa7589d744b4e9438164d1b84
matthew lee <matthewlee07@gmail.com>
2019-01-11 18:36:19 -0500

    successfully added winner&tie, replaced board with @board, still need to initialize @available_spots, disect eval_board & get_best_move

--- ttt/original.rb
+++ ttt/original.rb
@@ -10,25 +10,23 @@ class Game
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} \n===+===+===\n #{@board[3]} | #{@board[4]} | #{@board[5]} \n===+===+===\n #{@board[6]} | #{@board[7]} | #{@board[8]} \n"
     puts "Enter [0-8]:"
     # loop through until the game was won or tied
-    until game_is_over(@board) || tie(@board)
+    until winner || tie
       get_human_spot
-      if !game_is_over(@board) && !tie(@board)
+      if !winner && !tie
         eval_board
       end
       puts " #{@board[0]} | #{@board[1]} | #{@board[2]} \n===+===+===\n #{@board[3]} | #{@board[4]} | #{@board[5]} \n===+===+===\n #{@board[6]} | #{@board[7]} | #{@board[8]} \n"
     end
-    puts "Game over"
+    puts winner ? "game over: winner" : "game over: tie"
   end
 
   def get_human_spot
     spot = nil
     until spot
       spot = gets.chomp.to_i
-      # puts spot >= 0 && spot <= 8 ? "valid input" : "invalid input"
       if @board[spot] != "X" && @board[spot] != "O"
         @board[spot] = @hum
       else
-        # puts "unacceptable input"
         spot = nil
       end
     end
@@ -41,7 +39,7 @@ class Game
         spot = 4
         @board[spot] = @com
       else
-        spot = get_best_move(@board, @com)
+        spot = get_best_move
         if @board[spot] != "X" && @board[spot] != "O"
           @board[spot] = @com
         else
@@ -51,28 +49,28 @@ class Game
     end
   end
 
-  def get_best_move(board, next_player, depth = 0, best_score = {})
+  def get_best_move #(board, next_player, depth = 0, best_score = {})
     available_spaces = []
     get_available_spaces(available_spaces)
     best_move = nil
     available_spaces.each do |as|
-      board[as.to_i] = @com
-      if game_is_over(board)
+      @board[as.to_i] = @com
+      if winner
         best_move = as.to_i
-        board[as.to_i] = as
+        @board[as.to_i] = as
         return best_move
       else
-        board[as.to_i] = @hum
-        if game_is_over(board)
+        @board[as.to_i] = @hum
+        if winner
           best_move = as.to_i
-          board[as.to_i] = as
+          @board[as.to_i] = as
           return best_move
         else
-          board[as.to_i] = as
+          @board[as.to_i] = as
         end
       end
     end
-    best_move ? best_move : get_random_move(board)
+    best_move ? best_move : get_random_move(available_spaces) #(@board)
   end
 
   #receives board array, returns array of available spaces
@@ -82,35 +80,34 @@ class Game
         available_spaces << space
       end
     end
-    puts "available_spaces: #{available_spaces}"
     return available_spaces
   end
 
   #receives board array and best_move variable, returns boolean
-  def winning_move(board, best_move)
+  def winning_move
   end
 
   #receives board array and best_move variable, returns boolean
-  def block_winning_move(board, best)
+  def block_winning_move
   end
 
   def get_random_move(available_spaces)
     return available_spaces.sample.to_i
   end
 
-  def game_is_over(b)
-    [b[0], b[1], b[2]].uniq.length == 1 ||
-    [b[3], b[4], b[5]].uniq.length == 1 ||
-    [b[6], b[7], b[8]].uniq.length == 1 ||
-    [b[0], b[3], b[6]].uniq.length == 1 ||
-    [b[1], b[4], b[7]].uniq.length == 1 ||
-    [b[2], b[5], b[8]].uniq.length == 1 ||
-    [b[0], b[4], b[8]].uniq.length == 1 ||
-    [b[2], b[4], b[6]].uniq.length == 1
+  def winner
+    [@board[0], @board[1], @board[2]].uniq.length == 1 ||
+    [@board[3], @board[4], @board[5]].uniq.length == 1 ||
+    [@board[6], @board[7], @board[8]].uniq.length == 1 ||
+    [@board[0], @board[3], @board[6]].uniq.length == 1 ||
+    [@board[1], @board[4], @board[7]].uniq.length == 1 ||
+    [@board[2], @board[5], @board[8]].uniq.length == 1 ||
+    [@board[0], @board[4], @board[8]].uniq.length == 1 ||
+    [@board[2], @board[4], @board[6]].uniq.length == 1
   end
 
-  def tie(b)
-    b.all? { |space| space == "X" || space == "O" }
+  def tie
+    @board.all? { |space| space == "X" || space == "O" }
   end
 end
 
